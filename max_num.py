61f8705f8f724708f89328c79d69eb328e3aeb7f
anantterkar <82640594+anantterkar@users.noreply.github.com>
2021-08-25 19:49:14 +0530

    Add files via upload

---
+++ max_num.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 25 19:33:54 2021
+
+@author: USER
+"""
+
+#max of two numbers
+
+a = input("give a number\n")
+b = input("give another number\n")
+if(a>b):
+    print("Maximum number is:",a)
+else:
+   print("Maximum number is:",b)
